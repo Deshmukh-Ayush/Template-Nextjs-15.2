bf8479a698968ac4ee303c4e0bce43a1e7e8f75c
Ayush Deshmukh <ayushdeshmukh301@gmail.com>
2025-03-08 11:59:47 +0530

    minor change in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center">
-      <h1 className="text-5xl">Satverse HomePage</h1>
+      <h1 className="text-5xl">Template Homepage</h1>
       <p>This is just a initial page</p>
     </div>
   );
